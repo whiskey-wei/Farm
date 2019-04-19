add2bee89907bcfbbfa9920248a68e46fe12d54b
2016317200412 <31922114+2016317200412@users.noreply.github.com>
2019-04-19 15:40:19 +0800

    增加getpage,错误码

---
+++ pkg/e/code.go
@@ -0,0 +1,12 @@
+package e
+
+const (
+	SUCCESS        = 200
+	ERROR          = 500
+	INVALID_PARAMS = 400
+
+	ERROR_USER_CHECK_TOKEN_FAIL    = 20001
+	ERROR_USER_CHECK_TOKEN_TIMEOUT = 20002
+	ERROR_USER_TOKEN               = 20003
+	ERROR_USER                     = 20004
+)
--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -15,6 +15,9 @@ var (
 	WriteTimeout time.Duration
 
 	RunMode string
+
+	PageSize  int
+	JwtSecret string
 )
 
 func init() {
@@ -25,6 +28,7 @@ func init() {
 	}
 	LoadBase()
 	LoadServer()
+	LoadApp()
 }
 
 func LoadBase() {
@@ -41,4 +45,13 @@ func LoadServer() {
 	HttpPort = sec.Key("HTTP_PORT").MustString("8080")
 	ReadTimeout = time.Duration(sec.Key("READ_TIMEOUT").MustInt(60)) * time.Second
 	WriteTimeout = time.Duration(sec.Key("WRITE_TIMEOUT").MustInt(60)) * time.Second
+}
+
+func LoadApp() {
+	sec, err := Cfg.GetSection("app")
+	if err != nil {
+		log.Fatal("Fail to get section 'app':", err)
+	}
+	JwtSecret = sec.Key("JWT_SECRET").MustString("!@)*#)!@U#@*!@!)")
+	PageSize = sec.Key("PAGE_SIZE").MustInt(10)
 }
