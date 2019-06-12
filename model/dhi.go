42f9f4624b1b3c8e85af48109559609a6e3a8ea1
helewei <lewei.he@shopee.com>
2019-06-12 10:27:01 +0800

    conf

--- model/dhi.go
+++ model/dhi.go
@@ -2,7 +2,7 @@ package model
 
 type DhiRecord struct { //DHI取样
 	Id             int     `gorm:"primary_key" json:"id"`
-	YakId          int     `json:"Yak_id"`
+	YakId          int     `json:"yak_id"`
 	RecordTime     string  `json:"record_time"`
 	BirthTime      string  `json:"birth_time"`
 	DayAge         int     `json:"day_age"`
